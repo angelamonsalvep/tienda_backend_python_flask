49db667af3fa097eab4c5beaece7ec5c046495a2
Angela Monsalve <angelapmonsalve@gmail.com>
2025-09-22 04:41:17 -0500

    se agrega ajuste para que Flask escuche en todas las interfaces de red y en el puerto 5000.

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def bienvenida():
     return {'mensaje': 'Bienvenido a la API de Tienda Backend'}, 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+

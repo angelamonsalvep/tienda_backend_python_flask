eaef3704c6ccb5ff22c8e6af27327fe5982c4acc
Angela Monsalve <angelapmonsalve@gmail.com>
2026-06-09 14:24:24 -0500

    refactor: configure dynamic port binding using PORT environment variable

--- app.py
+++ app.py
@@ -29,5 +29,7 @@ def bienvenida():
     return {'mensaje': 'Bienvenido a la API de Tienda Backend'}, 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
